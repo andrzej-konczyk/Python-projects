7c8a391744002b8716f04a8fef469a5c95efd57a
Andrzej Kończyk <91786326+andrzej-konczyk@users.noreply.github.com>
2024-02-19 18:59:14 +0100

    Update RandezVousGenerator.py
    Code cleaning

--- RandezVousGenerator/Source code and img/RandezVousGenerator.py
+++ RandezVousGenerator/Source code and img/RandezVousGenerator.py
@@ -36,16 +36,13 @@ def generate_and_display_idea():
     idea = app.generate_idea()
     app.display_images(idea)
 
-# Pobranie ścieżki do katalogu, w którym znajduje się uruchamiany skrypt
 script_directory = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
 
-# Sprawdzenie, czy plik CSV istnieje w bieżącym katalogu lub w katalogu nadrzędnym
 csv_file_path = os.path.join(script_directory, "RandezVous.csv")
 if not os.path.exists(csv_file_path):
     parent_directory = os.path.abspath(os.path.join(script_directory, os.pardir))
     csv_file_path = os.path.join(parent_directory, "RandezVous.csv")
 
-# Zbudowanie ścieżki do folderu z obrazami
 images_folder_path = script_directory
 
 app = IdeaGeneratorApp(csv_file_path, images_folder_path)
@@ -53,10 +50,8 @@ app = IdeaGeneratorApp(csv_file_path, images_folder_path)
 root = tk.Tk()
 root.title("Generator pomysłu na randkę")
 
-# Zmiana koloru tła na odcień różu
 root.configure(bg="#FFCCCC")
 
-# Zmiana koloru przycisku na odcień czerwieni
 style = ttk.Style()
 style.configure("TButton", background="#FF3333", font=("Arial", 12))
 
@@ -66,7 +61,6 @@ generate_button.pack(pady=10)
 idea_label = ttk.Label(root, text="Wygenerowany pomysł na randkę:", font=("Arial", 12, "bold"))
 idea_label.pack()
 
-# Ustawienie stylu ramki, aby zmienić kolor tła
 style.configure("TFrame", background="#FFCCCC")
 
 images_frame = ttk.Frame(root)
